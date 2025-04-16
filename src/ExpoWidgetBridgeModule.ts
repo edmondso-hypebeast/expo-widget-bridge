141011ccef31e5c0729b279865bf71b7cb35aeab
edmondso <edmond.so@101medialab.com>
2025-04-16 14:51:24 +0800

    update: Update apply 'expo-modules-core' to use requireNativeModule

--- src/ExpoWidgetBridgeModule.ts
+++ src/ExpoWidgetBridgeModule.ts
@@ -1,4 +1,4 @@
-import { requireNativeModule } from "expo";
+import { requireNativeModule } from "expo-modules-core";
 
 // This call loads the native module object from the JSI.
 export default requireNativeModule("ExpoWidgetBridge");
